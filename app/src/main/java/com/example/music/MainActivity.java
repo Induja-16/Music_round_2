9b7aaaf519874bccfd0097c1d4328f59f49a1ae7
Induja-16 <rsinduja19@siesgst.ac.in>
2020-07-21 17:03:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    String s1[], s2[];
+    int images[]={R.drawable.temp1,R.drawable.temp2,R.drawable.temp3,R.drawable.temp4,R.drawable.temp5,R.drawable.temp6};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.templates);
+        s2 = getResources().getStringArray(R.array.descp);
+        MyAdapter myAdapter = new MyAdapter(this,s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+}
+
+
